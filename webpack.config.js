36c627f3c24c2209d82693d9507d1e57a5939250
Niyaz Garipov <Niayz_Indicus@mail.ru>
2023-03-31 16:59:47 +0300

    feat: create webpack.config file and add basic configuration

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const path = require('path');
+
+export default {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'source-map',
+  devServer: {
+    port: 3000,
+    hot: false,
+  },
+};
